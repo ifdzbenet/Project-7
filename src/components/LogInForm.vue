40c1595215eafa4b8edf0fd2be34c737925bd425
ifdzbenet <ifdzbenet@gmail.com>
2023-01-11 11:24:47 +0100

    completely revamped the interface of the log in and sign up page

--- src/components/LogInForm.vue
+++ src/components/LogInForm.vue
@@ -1,12 +1,12 @@
 <template>
     <form method="post">
         <label for="email">E-mail</label>
-        <input type="email" id="email" name="email"><br>
+        <input type="email" id="email" name="email">
 
         <label for="password">Password</label>
-        <input type="text" id="password" name="password"><br>
+        <input type="text" id="password" name="password">
         
-        <input type="submit" value="Submit">
+        <input type="submit" value="Submit" class="button">
     </form>
 </template>
 
@@ -19,7 +19,36 @@ export default {
 
 <style>
 form {
+    height: 100%;
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
+    flex-wrap: wrap;
+}
+
+.button {
+    width: 35%;
+    height: 2.5em;
+    border-style: none;
+    padding: 0;
+    border-radius: 10px;
+    font-family: Sans-Bold;
+    color: #FD2D01;
+}
+
+label {
+    float: left;
+    width: 30%;
+    height: 1.5em;
+    display: inline;
+    color: white;
+    font-family: Sans-Bold;
+    font-size: 14px;
+}
+
+input {
+    float: left;
+    width: 50%;
+    height: 1.5em;
+    display: inline;
 }
 </style>
--- src/views/LogInSignUp.vue
+++ src/views/LogInSignUp.vue
@@ -1,23 +1,41 @@
 <template>
     <div id="container">
-        <div id="logo-block" href="">
-            <img src="../assets/logos/icon-left-font.png">
-        </div>
         <div id="content">
-            <div id="button-container">
-                <button id="sign-up" @click="turnSignUp()" :class="{selectedButton: showSignUp}">
-                    <p>Sign up</p>
-                </button>
-                <button id="log-in" @click="turnLogIn()" :class="{selectedButton: showLogIn}">
-                    <p>Log in</p>
-                </button>
+            <div id="welcome-message">
+                <h2>Welcome to the</h2>
+                <img src="../assets/logos/icon-left-font-monochrome-white.svg" alt="Groupomania"> 
+                <h1>Internal Social Network</h1>
             </div>
             <div id="form-body">
                 <div id="form-sign-up" v-if="showSignUp">
-                    <SignUpForm />
+                    <form method="post">
+                        <label for="first-name">First Name</label>
+                        <input type="text" id="first-name" name="first-name">
+                        <div class="fake_hr"></div>
+                        <label for="last-name">Last Name</label>
+                        <input type="text" id="last-name"  name="last-name">
+                        <div class="fake_hr"></div>
+                        <label for="email">E-mail</label>
+                        <input type="email" id="email" name="email">
+                        <div class="fake_hr"></div>
+                        <label for="password">Password</label>
+                        <input type="text" id="password" name="password">
+                        <div class="fake_hr"></div>
+                        <input type="submit" value="Submit" class="button">
+                    </form>
+                    <p>Already have an account?<button class="toggle_button" @click="turnLogIn()">Log in</button></p>
                 </div>
                 <div id="form-log-in" v-if="showLogIn"> 
-                    <LogInForm />
+                    <form method="post">
+                        <label for="email">E-mail</label>
+                        <input type="email" id="email" name="email">
+                        <div class="fake_hr"></div>
+                        <label for="password">Password</label>
+                        <input type="text" id="password" name="password">
+                        <div class="fake_hr"></div>
+                        <input type="submit" value="Submit" class="button nottransparent">
+                    </form>
+                    <p>Need an account? <button class="toggle_button" @click="turnSignUp()">Sign up</button></p>
                 </div>
             </div>
         </div>
@@ -25,52 +43,41 @@
 </template>
 
 <script>
-import SignUpForm from '../components/SignUpForm.vue'
-import LogInForm from '../components/LogInForm.vue'
 
 export default {
     name: 'LogInSignUp',
     components: {
-        SignUpForm,
-        LogInForm
+
     },
     data(){
         return { 
           //Boolean to show the drop down
           showLogIn: true,
           showSignUp: false,
+          //welcomeMessage = true
         }
     },
     methods: {
     turnLogIn() {
         this.showSignUp = false
         this.showLogIn = true
+        //this.welcomeMessage = false
     },
     turnSignUp() {
         this.showSignUp = true
         this.showLogIn = false
+        //this.welcomeMessage = false
     },
     }
 }
 </script>
 
 <style scoped>
-#logo-block {
-  width: 16em;
-  height: 3.5em;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: white;
-  border-radius: 40%;
-  box-shadow: 0 2px 4px 0 #FFC90E, 0 6px 20px 0 rgba(0,0,0,0.19);
-}
-#logo-block img {
-  width: 17.5em;
-  height: 2.5em;
-  overflow: hidden;
-  object-fit: cover;
-  border-radius: 43%;
+
+p {
+    margin: 0;
+    font-size: 14px;
+    color: black;
 }
 
 #container {
@@ -84,46 +91,117 @@ export default {
 }
 
 #container #content {
-    height: 85%;
+    height: 100%;
+    width: 30%;
     display: flex;
     flex-direction: column;
-    justify-content: flex-start;
+    justify-content: space-around;
+    background-image: url("../assets/backgrounds/welcome_design.png");
+    background-repeat: no-repeat;
+    background-size: cover;
+    border-radius: 20px;
 }
 
-#button-container {
-    height: 11%;
+#welcome-message {
+    width: 100%;
+    height: 30%;
+    color: white;
     display: flex;
+    flex-direction: column;
     align-items: center;
-    justify-content: space-evenly;
-    background-color: none;
 }
-button {
-    width: 50%;
-    height: 100%;
+
+#welcome-message img {
+    width: 70%;
+}
+#welcome-message h1 {
+    width: 90%;
+    padding-top: 20px;
+    margin: 0;
+    font-size: 20px;;
+    display: flex;
+    overflow: hidden;
+    justify-content: center;
+}
+
+.button {
+    width: 35%;
+    height: 2.5em;
     border-style: none;
-    border-left-style: solid;
-    border-left-width: 0.1;
-    border-left-color: grey;
-    border-right-style: solid;
-    border-right-width: 0.1;
-    border-right-color: grey;
-    background-color: rgb(233, 233, 233);
     padding: 0;
+    background-color: white;
+    border-radius: 10px;
+    font-family: Sans-Bold;
+    color: #FD2D01;
+}
+
+.toggle_button {
+    width: 3.8em;
+    height: 2.5em;
+    border-style: none;
+    padding: 0;
+    background: transparent;
+    font-family: Sans-Bold;
+    color: #fff7E8;
 }
 .selectedButton {
     border-color: white;
     background-color: white;
 }
 
-button:hover {
-    border-color: white;
-    background-color: white;
+
+#form-body {
+    padding: 0 20px 0 20px;
+    height: 60%;
+}
+
+#form-log-in {
+    height: 70%;
+}
+
+#form-sign-up {
+    height: 100%;
+}
+
+form {
+    height: 80%;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+}
+
+label {
+    float: left;
+    width: 30%;
+    height: 1.5em;
+    display: inline;
+    color: white;
+    font-family: Sans-Bold;
+    font-size: 14px;
+}
+
+input {
+    float: left;
+    width: 50%;
+    height: 1.5em;
+    display: inline;
+    border-style: none;
+    outline: none;
+    background: transparent;
+    font-family: Sans-Regular;
+    color: #fff7E8;
 }
 
 
-#form-body {
+
+.nottransparent {
+    background: white;
+}
+
+.fake_hr {
+    width: 100%;
+    height: 2px;
     background-color: white;
-    padding: 1.5em;
-    height: 70%;
+    margin-top: -15px;
 }
 </style>
